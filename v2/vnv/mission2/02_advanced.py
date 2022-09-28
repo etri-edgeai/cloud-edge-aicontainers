6c254f2ea810a067494647a7469810d5961e1505
jpark <jpark@m14-jpark.local>
2022-09-28 13:08:06 +0900

    vnv

---
+++ v2/vnv/mission2/02_advanced.py
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# http://keticmr.iptime.org:22809
+
+# <ubuntu> 
+# $ sudo apt install speedtest-cli
+
+# <rpi> 
+# $ sudo pip3 install speedtest-cli
+
+import subprocess
+import time
+
+# This is our shell command, executed by Popen.
+
+def run(cmd, is_show=False):
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+
+    if(is_show):
+        print(p.communicate())
+
+
+print('[+] Start advanced experiment')
+#---------------------------------------------------
+start = time.time()
+#---------------------------------------------------
+
+cmd = 'ansible vnv -m shell -a "cat /proc/cpuinfo" -i hosts.ini > ./tmp/baseline_cpuinfo.txt'
+run(cmd, False)
+
+cmd = 'ansible vnv -m shell -a "cat /proc/meminfo" -i hosts.ini > ./tmp/baseline_meminfo.txt'
+run(cmd, False)
+
+#---------------------------------------------------
+end = time.time()
+#---------------------------------------------------
+
+# print('t = ', end - start)
+print('[+] Done advanced experiment')
