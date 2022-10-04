4e49446be1300c4bb3d5d666a9519a97172e3c41
jpark <jpark@m14-jpark.local>
2022-10-04 21:04:48 +0900

    vnv

--- v2/vnv/mission2/run_vnv.py
+++ v2/vnv/mission2/run_vnv.py
@@ -44,18 +44,23 @@ def main(mode = 'baseline'):
     # 에지 디바이스의 상태정보를 얻습니다.
     #----------------------------------
 
-    wdir = '/home/jpark/WorkDevEdgeAI/cloud-edge-aicontainers/v2/vnv/mission2/'
-    py = '/usr/bin/python3'
+    wdir = ' /home/jpark/WorkDevEdgeAI/cloud-edge-aicontainers/v2/vnv/mission2/'
+    py = ' /usr/bin/python3'
     selected_model = 'resnet152' # default
     device = 'cuda'
     N = 10
     ask_pass_option = '' #  '--ask-become-pass'
     model_selector = ModelSelection()
 
+    print( wdir )
+    print( py )
+    print( selected_model )
+    print(' ')
     #cmd_sub = ' /usr/bin/python3 -c "import torch; print(torch.cuda.is_available())" '
-    cmd = f'ansible vnv -m shell -a "cd {wdir}; pwd; {py} cuda_is_available.py " -i hosts.ini '
+    cmd = f'ansible vnv -m shell -a "cd {wdir}; {py} cuda_is_available.py " -i hosts.ini '
     run(cmd, True)
 
+    # for ubuntu
     cmd = f'ansible vnv -m shell -a "cat /proc/cpuinfo" -i hosts.ini > ./tmp/baseline_cpuinfo.txt'
     run(cmd, False)
 
