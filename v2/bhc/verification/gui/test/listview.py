562a86558d1e87905d4b0d43d1347e1f45a3d2d6
ethicsense <ethicsense@DESKTOP-1F4AF91>
2022-12-01 17:12:11 +0900

    QListView test on progress

---
+++ v2/bhc/verification/gui/test/listview.py
@@ -0,0 +1,34 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+from PyQt5 import QtCore, QtGui, QtWidgets
+import requests
+
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        self.Registry_list = QtWidgets.QListView(Dialog)
+        self.Registry_list.setGeometry(QtCore.QRect(20, 160, 231, 251))
+        self.Registry_list.setObjectName("Registry_list")
+        
+        search = requests.get('{url}/v2/_catalog'.format(url='http://172.26.64.1:5000'))
+        nodes = eval(search.text)
+        nodes = nodes['repositories']
+        model = QStandardItemModel()
+        for n in nodes:
+            model.appendRow(QStandardItem(n))
+        self.Registry_list.setModel(model)
+
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
