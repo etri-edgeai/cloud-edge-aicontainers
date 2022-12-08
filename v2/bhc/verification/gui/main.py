15d8081665e1b414395749a10fab4941944c5ff5
ethicsense <ethicsense@DESKTOP-1F4AF91>
2022-12-08 17:52:36 +0900

    main-window rebuilding

--- v2/bhc/verification/gui/main.py
+++ v2/bhc/verification/gui/main.py
@@ -3,104 +3,85 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from PyQt5 import QtCore, QtGui, QtWidgets
+
 import requests
 
-class Ui_Dialog(object):
+class Main(QWidget):
 
-    def setupUi(self, Dialog):
-        ## layout
-        Dialog.setObjectName("Dialog")
-        Dialog.resize(486, 473)
-        self.comboBox = QtWidgets.QComboBox(Dialog)
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        
+        # repo list
+        self.comboBox = QComboBox(self)
         self.comboBox.setGeometry(QtCore.QRect(30, 60, 111, 31))
-        self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-
-        self.label = QtWidgets.QLabel(Dialog)
-        self.label.setGeometry(QtCore.QRect(30, 30, 101, 21))
+        self.comboBox.addItem("aarch64")
+        self.comboBox.addItem("x86")
+        self.comboBox.addItem("x64")
+        self.comboBox.addItem("x86_64")
+        # texts
+        label = QLabel('label1', self)
+        label.setGeometry(QtCore.QRect(30, 30, 101, 21))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(True)
         font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
+        label.setFont(font)
 
-        self.label_2 = QtWidgets.QLabel(Dialog)
-        self.label_2.setGeometry(QtCore.QRect(170, 30, 91, 21))
+        label2 = QLabel('label2', self)
+        label2.setGeometry(QtCore.QRect(170,30,91,21))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(True)
         font.setWeight(75)
-        self.label_2.setFont(font)
-        self.label_2.setObjectName("label_2")
+        label2.setFont(font)
 
-        self.lineEdit = QtWidgets.QLineEdit(Dialog)
+        self.lineEdit = QLineEdit(self)
         self.lineEdit.setGeometry(QtCore.QRect(170, 60, 121, 31))
-        self.lineEdit.setObjectName("lineEdit")
 
-        self.buttonBox = QtWidgets.QDialogButtonBox(Dialog)
+        self.buttonBox = QDialogButtonBox(self)
         self.buttonBox.setGeometry(QtCore.QRect(320, 60, 131, 31))
         self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setCenterButtons(True)
-        self.buttonBox.setObjectName("buttonBox")
 
-        self.repo_list = QtWidgets.QListWidget(Dialog)
-        self.repo_list.setGeometry(QtCore.QRect(20, 160, 231, 251))
-        self.repo_list.setObjectName("repo_list")
+        repo_list = QListWidget(self)
+        repo_list.setGeometry(QtCore.QRect(20,160,231,251))
         search = requests.get('{url}/v2/_catalog'.format(url='http://172.26.64.1:5000'))
         nodes = eval(search.text)
         nodes = nodes['repositories']
         for n in nodes:
-            self.repo_list.addItem(str(n))
+            repo_list.addItem(str(n))
 
-        self.label_3 = QtWidgets.QLabel(Dialog)
-        self.label_3.setGeometry(QtCore.QRect(50, 130, 161, 20))
+        label3 = QLabel('label3', self)
+        label3.setGeometry(QtCore.QRect(50,130,161,20))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(True)
         font.setWeight(75)
-        self.label_3.setFont(font)
-        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_3.setObjectName("label_3")
+        label3.setFont(font)
+        label3.setAlignment(QtCore.Qt.AlignCenter)
 
-        self.label_4 = QtWidgets.QLabel(Dialog)
-        self.label_4.setGeometry(QtCore.QRect(290, 159, 141, 31))
+        label4 = QLabel('label4', self)
+        label4.setGeometry(QtCore.QRect(290,159,141,31))
         font = QtGui.QFont()
         font.setPointSize(12)
         font.setBold(True)
         font.setWeight(75)
-        self.label_4.setFont(font)
-        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_4.setObjectName("label_4")
+        label4.setFont(font)
+        label4.setAlignment(QtCore.Qt.AlignCenter)
 
-        self.model_list = QtWidgets.QListView(Dialog)
-        self.model_list.setGeometry(QtCore.QRect(270, 200, 191, 211))
-        self.model_list.setObjectName("model_list")
+        self.model_list = QListView(self)
+        self.model_list.setGeometry(QtCore.QRect(270,200,191,211))
 
-        ## signal connect
-        self.retranslateUi(Dialog)
-        QtCore.QMetaObject.connectSlotsByName(Dialog)
-        self.repo_list.itemClicked.connect(self.show_models)
+        self.setGeometry(300, 300, 488, 493)
+        self.setWindowTitle('Edge Tool v0.1')
+        repo_list.itemClicked.connect(self.show_models)
         self.buttonBox.clicked.connect(self.verify)
-
-    ## edit texts
-    def retranslateUi(self, Dialog):
-        _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
-        self.comboBox.setItemText(0, _translate("Dialog", "aarch64"))
-        self.comboBox.setItemText(1, _translate("Dialog", "x86"))
-        self.comboBox.setItemText(2, _translate("Dialog", "x64"))
-        self.comboBox.setItemText(3, _translate("Dialog", "x86_64"))
-        self.label.setText(_translate("Dialog", "MODEL"))
-        self.label_2.setText(_translate("Dialog", "TASK"))
-        self.label_3.setText(_translate("Dialog", "REPOSITORIES"))
-        self.label_4.setText(_translate("Dialog", "IMAGE LIST"))
-
-    ## ================= functions =================
-
+        self.show()
+            
+        
     def show_models(self, item):
         search2 = requests.get('{url}/v2/{repo}/tags/list'.format(url='http://172.26.64.1:5000', repo=str(item.text())))
         tags = eval(search2.text)
@@ -109,6 +90,9 @@ class Ui_Dialog(object):
         for n in tags:
             model.appendRow(QStandardItem(n))
         self.model_list.setModel(model)
+
+    def msgVerify(self, result):
+        QMessageBox.information(self, "ALERT", result)
 
     def verify(self):
         reg_url = "http://172.26.64.1:5000"
@@ -124,21 +108,21 @@ class Ui_Dialog(object):
 
             if task in search2.text:
                 image_name = "{model}:{task}".format(model=model, task=task)
-                print('we already have the model.')
-                print("download command : 'docker pull {url}/{image_name}'".format(url=reg_url, image_name=image_name))
+                result = """
+We already have the model.
 
+< download command >
+docker pull {url}/{image_name}
+                """.format(url=reg_url, image_name=image_name)
+                self.msgVerify(result)
+
+                
             else:
-                print('activate distribution sequence.')
+                result = 'We need to build a new one. Please activate distribution sequence.'
+                QtWidgets.QMessageBox.information(self, "NOTICE", result)
 
 
-
-## activation
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
-    Dialog = QtWidgets.QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(Dialog)
-    Dialog.show()
+    ex = Main()
     sys.exit(app.exec_())
-
