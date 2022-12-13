702a1a7b0190d05bca9436d3c46f0255338a2772
ethicsense <ethicsense@DESKTOP-1F4AF91>
2022-12-13 13:23:34 +0900

    testing upload.py on progress | update README.md

--- v2/bhc/verification/gui/test/buildTest/upload.py
+++ v2/bhc/verification/gui/test/buildTest/upload.py
@@ -1,5 +1,7 @@
 import sys
 from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
 class MyWindow(QMainWindow):
     def __init__(self):
@@ -8,24 +10,33 @@ class MyWindow(QMainWindow):
 
     def setupUI(self):
         self.setGeometry(800, 200, 500, 300)
-
-        btn1 = QPushButton("load",self)
+        btn1 = QPushButton("upload files", self)
+        btn1.setGeometry(390, 50, 100, 30)
         btn1.clicked.connect(self.btn_fun_FileLoad)
-        btn2 = QPushButton('copy', self)
+        
+        btn2 = QPushButton('copy files', self)
+        btn2.setGeometry(390, 100, 100, 30)
         btn2.clicked.connect(self.copy_files)
+        self.fileview = QListView(self)
+        self.fileview.setGeometry(10, 50, 370, 180)
+
 
     def btn_fun_FileLoad(self):
         fname = QFileDialog.getOpenFileNames(self)
+        model = QStandardItemModel()
         for f in fname[0]:
             if 'Dockerfile' in f:
                 Dockerfile_path = f
+                model.appendRow(QStandardItem(Dockerfile_path))
             else:
                 zipModel_path = f
-        print(Dockerfile_path)
-        print(zipModel_path)
+                model.appendRow(QStandardItem(zipModel_path))
+        self.fileview.setModel(model)
+
 
     def copy_files(self):
         pass
+
 
         
 
