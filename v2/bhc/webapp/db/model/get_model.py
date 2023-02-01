adea610a1af1dd23e1c822d3284ed9898636f60f
ethicsense <gycksdl94@gmail.com>
2023-02-01 13:00:41 +0900

    rename pred codes | get_model_information on progress

--- v2/bhc/webapp/db/model/get_model.py
+++ v2/bhc/webapp/db/model/get_model.py
@@ -1,15 +1,23 @@
 import sqlite3
-
+import json
 import os
 import re
 
+def get_model_info():
+    
+    os.system("curl -s https:/123.214.186.252:39500/v2/_catalog -k > modelog.txt")
 
+    with open('modelog.txt', 'r') as f:
+        data = json.load(f)
+    
+    print(type(data['repositories'][0]))
 
 def model_download():
     pass
 
-
+def get_desc():
+    pass
 
 if __name__ == "__main__":
 
-    model_download()
+    get_model_info()
