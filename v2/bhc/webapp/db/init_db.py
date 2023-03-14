d58d3e279ff646ac5fe446bbabf92659ed5011db
ethicsense <gycksdl94@gmail.com>
2023-03-14 18:00:00 +0900

    device manage interface test on progress

--- v2/bhc/webapp/db/init_db.py
+++ v2/bhc/webapp/db/init_db.py
@@ -32,15 +32,17 @@ def main(file_path):
     database = file_path
 
     nodes_table = """ CREATE TABLE IF NOT EXISTS nodes (
-                        time NOT NULL,
-                        group text NOT NULL,
-                        name text NOT NULL,
-                        type text NOT NULL,
-                        owner text NOT NULL,
-                        hw text,
-                        os text,
-                        gpu text
-                    );"""
+                    time NOT NULL,
+                    affiliation text NOT NULL,
+                    name text PRIMARY KEY,
+                    ip text NOT NULL,
+                    port text NOT NULL,
+                    type text NOT NULL,
+                    owner text NOT NULL,
+                    hw text,
+                    os text,
+                    gpu text
+                );"""
 
     location_table = """ CREATE TABLE IF NOT EXISTS location (
                         name text NOT NULL,
