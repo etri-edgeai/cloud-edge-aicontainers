808e78d5e0a3e13c506a411b08325b2a69c1de48
ethicsense <gycksdl94@gmail.com>
2023-04-05 17:48:13 +0900

    ai model deletion, ai model download on progress

--- v2/bhc/webapp/run_m_model.sh
+++
@@ -1,1 +0,0 @@
-python m_model.py --owner test --task classification --model_name imagenet --version v1.0 --model_file model.tar.gz --builder rpi6401 --repo aarch64-model --mode register
---
+++ v2/bhc/webapp/run_modelmanager.sh
@@ -0,0 +1,10 @@
+python m_model.py \
+--owner test2 \
+--task detection \
+--model_name yolov5 \
+--version v1.0 \
+--model_file model.tar.gz \
+--builder rpi6401 \
+--user rpi6402 \
+--repo aarch64-model \
+--mode download
