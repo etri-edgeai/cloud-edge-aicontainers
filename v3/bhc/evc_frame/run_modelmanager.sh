425e9e4d96620517013c40c0f608d6e775a11463
ethicsense <gycksdl94@gmail.com>
2023-07-07 08:55:04 +0000

    migration implementation

--- v3/bhc/evc_frame/run_modelmanager.sh
+++ v3/bhc/evc_frame/run_modelmanager.sh
@@ -1,10 +1,10 @@
 python m_model.py \
 --owner keti \
 --task detection \
---model_name RunTest_pd \
+--model_name gitlink_test2_pd \
 --version v1.0 \
 --model_file model.tar.gz \
 --builder rpi6401 \
 --user rpi6402 \
 --repo aarch64-model \
---mode delete
+--mode db_config
