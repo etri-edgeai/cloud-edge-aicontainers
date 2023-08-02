a2c595d047dc6ef1272f53064fceecc1dd44e20c
jpark <jpark@keti.re.kr>
2023-08-03 01:42:15 +0900

    nginx4evc

--- v3/login/config.php
+++ v3/login/config.php
@@ -3,9 +3,10 @@ define("DB_SERVER", "db");
 define("DB_USERNAME", "evc");
 define("DB_PASSWORD", "evc");
 define("DB_NAME", "evc");
+define("DB_PORT", 3300);
 
 # Connection
-$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
 
 # Check connection
 if (!$link) {
