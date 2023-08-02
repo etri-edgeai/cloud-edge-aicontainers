68b34d4d4be884e41639a900c9bdbfc8456a34b1
jpark <jpark@keti.re.kr>
2023-08-02 14:03:01 +0900

    update

--- v3/newedge.sh
+++ v3/newedge.sh
@@ -1,8 +1,77 @@
+#!/bin/bash
+
+#--------------------------------------------------
+# newedge.sh
+# 
+# - usage :
+#
+#   wget http://evc.re.kr/newedge.sh -O newedge.sh
+#   bash newedge.sh
+#  
+#
+# J. Park, KETI, 2023 
+#--------------------------------------------------
+
 # add edge device
-echo 'install openssh-server'
-apt update
-apt upgrade
-apt install openssh-server
-echo "IPQoS cs0 cs0" >> /etc/ssh/sshd_config
-cat /etc/ssh/sshd_config
+function rpi {
+    echo "Raspberry PI"
+    sudo apt update
+    sudo apt upgrade
+    echo 'install openssh-server'
+    sudo apt install openssh-server
+    echo "IPQoS cs0 cs0" | sudo tee --append /etc/ssh/sshd_config
+    sudo cat /etc/ssh/sshd_config
+}
+
+function ubuntu_focal {
+    echo "ubuntu_focal"
+    sudo apt update
+    sudo apt upgrade
+    sudo apt install openssh-server
+    echo "IPQoS cs0 cs0" | sudo tee --append /etc/ssh/sshd_config
+    sudo cat /etc/ssh/sshd_config
+    
+    curl -X GET http://evc.re.kr/api/get_key.php >> ~/.ssh/authorized_keys
+}
+
+
+OS=$(uname)
+#echo "$OS"
+
+ARCH=$(uname -m)
+#echo "$ARCH"
+
+if [[ "$OS" == 'Linux' ]]; then
+    platform='linux'
+   
+    CODE=$(lsb_release -c -s)
+    if [[ "$CODE" == 'lunar' ]]; then
+       platform='rpi_lunar'
+    elif [[ "$CODE" == 'bullseye' ]]; then
+       platform='rpi_bullseye'
+    elif [[ "$CODE" == 'focal' ]]; then
+       platform='ubuntu_focal'
+    fi   
+elif [[ "$OS" == 'FreeBSD' ]]; then
+    platform='freebsd'
+elif [[ "$OS" == 'Darwin' ]]; then
+    platform='macos'
+fi
+
+echo $platform
+
+if [[ "$platform" == 'rpi_lunar' ]]; then
+    rpi
+elif [[ "$platform" == 'rpi_bullseye' ]]; then
+    rpi
+elif [[ "$platform" == 'ubuntu_focal' ]]; then
+    ubuntu_focal
+elif [[ "$platform" == 'FreeBSD' ]]; then
+    echo 'todo'
+elif [[ "$platform" == 'macos' ]]; then
+    echo 'todo'
+fi
+
+
+
 
