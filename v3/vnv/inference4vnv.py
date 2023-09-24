909206db42b2e73a56a0c2b9bc0c2a90ad93f54b
jpark <jpark@keti.re.kr>
2023-09-24 20:04:35 +0900

    update

--- v3/vnv/inference4vnv.py
+++ v3/vnv/inference4vnv.py
@@ -335,8 +335,9 @@ if __name__ == "__main__":
     model_names.append( args.model )
     devices=[]
     devices.append( args.device )
-    mode = args.mode
     N = int( int(args.N) )
+    mode = args.mode
+    fpath_testimages = args.fpath_testimages
     
     # core
     run_main(model_names = model_names, devices=devices, N = N, mode = mode, fpath_testimages = fpath_testimages )
