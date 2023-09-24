7fe3b2a63323e594b32ab55cfb79b7dd49acb6ce
jpark <jpark@keti.re.kr>
2023-09-24 23:43:14 +0900

    update

--- v3/vnv/inference4vnv.py
+++ v3/vnv/inference4vnv.py
@@ -160,7 +160,7 @@ def run_main(model_names=['mobilenet_v3_small'], mode='baseline', fpath_testimag
     #    
     #n = len(testset)
     
-    if device_info['is_cuda_available'] == 'True':
+    if device_info['num_of_cuda_devices'] > 0:
         device = 'cuda'
     else: 
         device = 'cpu'
