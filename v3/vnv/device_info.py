a981606132f4c8787e7809288ac9dce32d6688b8
jpark <jpark@keti.re.kr>
2023-09-24 22:23:53 +0900

    update

--- v3/vnv/device_info.py
+++ v3/vnv/device_info.py
@@ -26,7 +26,8 @@ if __name__ == "__main__":
     
     od = OrderedDict({
         'hostname':hostname,
-        'is_cuda_available':str(is_cuda_available),
+        'cuda_is_available':str(is_cuda_available),
+        'cuda_num_of_devices':str(num_of_cuda_devices),
         'num_of_cuda_devices':num_of_cuda_devices,
         'platform':platform.system(),
         'arch':platform.machine(),
