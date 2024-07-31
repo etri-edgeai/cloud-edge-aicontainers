cf193602a0e2df5c9ce1a8f97fd118ed15102ae2
jpark <jpark@keti.re.kr>
2024-07-31 17:13:51 +0900

    update demo scenario code

--- run_git_push.sh
+++ run_git_push.sh
@@ -2,6 +2,7 @@ git pull
 git add .
 git status
 #git commit -m "code refactoring"
-git commit -m "Demo scenario for smart city"
+#git commit -m "Demo scenario for smart city"
+git commit -m "update demo scenario code"
 git push
 
