de3fcca0e5c4012371e0b4808bae07140315f135
jpark <jpark@keti.re.kr>
2024-08-05 11:35:34 +0900

    Development of AI containers components

--- evc/containers/hello_singleworld/run_build.sh
+++ evc/containers/hello_singleworld/run_build.sh
@@ -2,7 +2,7 @@
 docker build -t helloworld_single-python-app .
 
 # 이미지 확인
-docker images
+docker images | grep helloworld_single-python-app
 
 # 컨테이너 실행
 docker run --rm helloworld_single-python-app
