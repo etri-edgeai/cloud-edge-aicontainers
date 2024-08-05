ee84f9b4f9e317ce97cf4219db865cb77db76591
jpark <jpark@keti.re.kr>
2024-08-05 13:29:55 +0900

    Development of AI containers components

--- evc/containers_for_evc/beacon/beacon_x86.sh
+++ evc/containers_for_evc/beacon/beacon_x86.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 echo "Running on x86 architecture"
 # x86-specific commands here
+
+#------------------------------------------------
+# - Usage
+#     nohup bash beacon.sh &
+#
+# - by JPark @ KETI, 2023
+#
+# - Examples of vcgencmd
+#   vcgencmd version
+#   vcgencmd commands
+#   vcgencmd get_mem arm
+#   vcgencmd get_mem gpu
+#   vcgencmd measure_volts
+#   vcgencmd measure_temp
+#   vcgencmd measure_clock arm
+#------------------------------------------------
+
+hostname=$(uname -n)
+
+while true; do
+    temperature=$((paste <(cat /sys/class/thermal/thermal_zone*/type) <(cat /sys/class/thermal/thermal_zone*/temp) | column -s $'\t' -t | sed 's/\(.\)..$/.\1°C/' | grep x86_pkg_temp) | grep -oE '[0-9]+(\.[0-9]+)?' | tail -1)
+    cpuclock=$(lscpu | grep 'CPU MHz' | grep -oE '[0-9]+(\.[0-9]+)?')
+    mem=$(free -m | awk 'NR==2{printf "%s", $3 }')
+    #temperature=1.1
+    #cpuclock=1
+    #mem=2
+    curl -X 'GET' http://evc.re.kr:20080/puship.php?hostname=${hostname}\&temperature=${temperature}\&cpuclock=${cpuclock}\&mem=${mem}
+    echo ${temperature}
+    echo ${hostname}, ${temperature}, ${cpuclock}, ${mem}
+    sleep 60
+done
