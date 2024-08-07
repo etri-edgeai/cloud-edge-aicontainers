85d0d7868605ffdec5a49b9c429a9f8d1e39dd12
jpark <jpark@keti.re.kr>
2024-08-07 13:14:03 +0900

    Development of AI containers components

--- evc/containers_for_evc/beacon/beacon.sh
+++ evc/containers_for_evc/beacon/beacon.sh
@@ -1,21 +1,10 @@
 #!/bin/sh
-
 #------------------------------------------------
 # - Usage
 #     nohup bash beacon.sh &
 #
 # - by JPark @ KETI, 2024
-#
-# - Examples of vcgencmd
-#   vcgencmd version
-#   vcgencmd commands
-#   vcgencmd get_mem arm
-#   vcgencmd get_mem gpu
-#   vcgencmd measure_volts
-#   vcgencmd measure_temp
-#   vcgencmd measure_clock arm
 #------------------------------------------------
-
 hostname=$1
 
 echo "Beacon v1.1"
@@ -27,17 +16,17 @@ while true; do
     cpuclock=$(lscpu | grep "CPU max MHz" | awk '{print $4}')
 
     # Get available memory
-    mem_max=$(free -m | awk 'NR==2{print $2}')
+    mem_total=$(free -m | awk 'NR==2{print $2}')
     
     # Get available memory
     mem_available=$(free -m | awk 'NR==2{print $7}')
 
     # Send data to server
-    /usr/bin/curl "http://evc.re.kr:20080/puship.php?hostname=${hostname}&temperature=${temperature}&cpuclock=${cpuclock}&mem=${mem_available}"
+    /usr/bin/curl "http://evc.re.kr:20080/beacon.php?hostname=${hostname}&temperature=${temperature}&cpuclock=${cpuclock}&mem_total=${mem_total}&mem_avilable=${mem_available}"
 
     # Print data to console
-    echo "${hostname}, ${temperature}, ${cpuclock}, ${mem_max}, ${mem_available}"
+    #echo "${hostname}, ${temperature}, ${cpuclock}, ${mem_max}, ${mem_available}"
     
     # Wait for 60 seconds before next iteration
-    sleep 1
+    sleep 60
 done
